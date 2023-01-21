ac5f4d022bf51cb9a13522284f29bface86b0f76
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2023-01-21 10:27:48 +0200

    feat: add tw

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import {Inter} from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({subsets: ['latin']})
 
 export default function Home() {
   return (
@@ -16,6 +16,10 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.description}>
+          <h1 className="text-3xl font-bold underline">
+            Hello world!
+          </h1>
+
           <p>
             Get started by editing&nbsp;
             <code className={styles.code}>pages/index.tsx</code>
